036423797f08419aeadf829004e317893a6e5334
Aaron Barnard <abarnard@protonmail.com>
2022-08-07 15:57:52 +1000

    Update pre commit hooks

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,4 @@
 export default {
-	'**/*.{ts,svelte}': 'tsc -p tsconfig.json --noEmit',
-	'**/*.{js,ts,svelte,html,css}': 'prettier --ignore-unknown --write',
-	'**/*.{js,ts,svelte,html,css}': 'eslint --fix'
+	'**/*.{ts,svelte}?(x)': () => 'tsc -p tsconfig.json --noEmit',
+	'**/*.{js,ts,svelte,html,css,mjs}': 'eslint --fix'
 }
