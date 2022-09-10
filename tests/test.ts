f6c316e8c08156dae3f8eb28286796bd21e5f3aa
johngribbin <30157175+johngribbin@users.noreply.github.com>
2022-09-10 13:44:19 -0400

    Rename test

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,13 @@
 import { expect, test } from '@playwright/test'
 
-test('Welcome page has expected h1', async ({ page }) => {
+test('Index page has expected h1', async ({ page }) => {
   await page.goto('/')
 
   expect(await page.textContent('h1')).toBe('Welcome')
+})
+
+test('Logged out user gets kicked to welcome page', async ({ page }) => {
+  await page.goto('/settings')
+
+  await expect(page).toHaveURL('/welcome')
 })
