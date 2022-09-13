5af909ee338ec774a99eebcbb1777d011cbae9d0
Aaron <abarnard@protonmail.com>
2022-09-13 20:43:18 +1000

    Use basic ssl only in dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { readFileSync } from 'fs'
@@ -7,12 +8,15 @@ const file = fileURLToPath(new URL('package.json', import.meta.url))
 const json = readFileSync(file, 'utf8')
 const pkg = JSON.parse(json)
 
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [sveltekit(), basicSsl()],
-  define: {
-    __APP_VERSION__: JSON.stringify(pkg.version)
-  }
-}
+export default defineConfig(({ command }) => {
+  const devPlugins = command === 'serve' ? [basicSsl()] : []
 
-export default config
+  const config = {
+    plugins: [sveltekit(), ...devPlugins],
+    define: {
+      __APP_VERSION__: JSON.stringify(pkg.version)
+    }
+  }
+
+  return config
+})
