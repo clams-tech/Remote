906af82e77de6498d6a6b8ed35764e89832bed90
johngribbin <30157175+johngribbin@users.noreply.github.com>
2022-10-15 12:07:21 -0400

    Tweak darkmode css for summary row

--- src/lib/elements/SummaryRow.svelte
+++ src/lib/elements/SummaryRow.svelte
@@ -11,7 +11,7 @@
     ? 'center'
     : 'baseline'} border-b border-b-neutral-200 dark:border-b-neutral-700 py-3"
 >
-  <span class="mr-2 text-neutral-800 w-1/2"><slot name="label" /></span>
+  <span class="mr-2 text-neutral-800 dark:text-neutral-50 w-1/2"><slot name="label" /></span>
 
   <p class="flex items-center justify-end w-1/2">
     <slot name="value" />
