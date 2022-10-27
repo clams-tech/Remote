c56404fc3c4b57f9a4dd6ca14a48ef3d36567bf7
Aaron <abarnard@protonmail.com>
2022-10-27 19:52:30 +1100

    Fix word overflow and edit copy

--- src/lib/elements/SummaryRow.svelte
+++ src/lib/elements/SummaryRow.svelte
@@ -11,7 +11,9 @@
     ? 'baseline'
     : 'center'} border-b border-b-neutral-200 dark:border-b-neutral-700 py-3"
 >
-  <span class="mr-2 text-neutral-800 dark:text-neutral-200 w-1/3"><slot name="label" /></span>
+  <span class="mr-2 text-neutral-800 dark:text-neutral-200 w-1/3 whitespace-nowrap"
+    ><slot name="label" /></span
+  >
 
   <p class="flex items-center justify-end w-2/3">
     <slot name="value" />
