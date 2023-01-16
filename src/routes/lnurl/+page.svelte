abb0dabadba8acc3c14be22cb3b1c7386c9a7366
Aaron <abarnard@protonmail.com>
2023-01-17 08:12:35 +1100

    1.2.0-0.3.0 - Feature: LNURL Withdraw (#91)
    * Support lightning address
    
    * Move lightning address decode to general utils
    
    * Some formatting
    
    * Add lightning address to placeholder
    
    * Increment version
    
    * Add support for LNURL Withdraw

--- src/routes/lnurl/+page.svelte
+++ src/routes/lnurl/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { decodelnurl } from 'js-lnurl'
   import { translate } from '$lib/i18n/translations'
-  import Spinner from '$lib/elements/Spinner.svelte'
   import Auth from './components/auth.svelte'
-  import type { PageData } from './$types'
   import Pay from './components/pay.svelte'
-  import { LNURL_PROXY } from '$lib/constants'
+  import Withdraw from './components/withdraw.svelte'
   import warning from '$lib/icons/warning'
+  import Spinner from '$lib/elements/Spinner.svelte'
+  import { LNURL_PROXY } from '$lib/constants'
   import { decodeLightningAddress } from '$lib/utils'
+  import type { PageData } from './$types'
 
   export let data: PageData
 
@@ -21,8 +22,11 @@
   let callback: string // The URL from LN SERVICE which will accept the pay request parameters
   let maxSendable: number // Max millisatoshi amount LN SERVICE is willing to receive
   let minSendable: number // Min millisatoshi amount LN SERVICE is willing to receive, can not be less than 1 or more than `maxSendable`
+  let minWithdrawable: number // Min amount (in millisatoshis) the user can withdraw from LN SERVICE, or 0
+  let maxWithdrawable: number // Max amount (in millisatoshis) the user can withdraw from LN SERVICE, or equal to minWithdrawable if the user has no choice over the amounts
   let metadata: string // Metadata json which must be presented as raw string here, this is required to pass signature verification at a later step
   let commentAllowed: number // length of comment allowed
+  let defaultDescription: string // A default withdrawal invoice description
 
   getUrlDetails()
 
@@ -59,10 +63,12 @@
         callback = result.callback
         maxSendable = result.maxSendable
         minSendable = result.minSendable
+        maxWithdrawable = result.maxWithdrawable
+        minWithdrawable = result.minWithdrawable
         metadata = result.metadata
         commentAllowed = result.commentAllowed
+        defaultDescription = result.defaultDescription
         parsingLnurl = false
-        commentAllowed = result.commentAllowed
       } else {
         k1 = url.searchParams.get('k1') || ''
         action = url.searchParams.get('action') || 'login'
@@ -92,7 +98,9 @@
 {:else if tag === 'login'}
   <Auth {action} {url} {k1} />
 {:else if tag === 'payRequest'}
-  <Pay {url} {callback} {maxSendable} {minSendable} {metadata} {commentAllowed} />
+  <Pay {url} {callback} {minSendable} {maxSendable} {metadata} {commentAllowed} />
+{:else if tag === 'withdrawRequest'}
+  <Withdraw {url} {callback} {k1} {minWithdrawable} {maxWithdrawable} {defaultDescription} />
 {:else}
   <section class="w-full p-6 max-w-lg">
     <div class="flex text-utility-error">
--- src/routes/lnurl/components/withdraw.svelte
+++ src/routes/lnurl/components/withdraw.svelte
@@ -1,2 +1,241 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
+  import Amount from '$lib/components/Amount.svelte'
+  import Summary from '$lib/components/Summary.svelte'
+  import { LNURL_PROXY } from '$lib/constants'
+  import { convertValue } from '$lib/conversion'
+  import Button from '$lib/elements/Button.svelte'
+  import ErrorMsg from '$lib/elements/ErrorMsg.svelte'
+  import Slide from '$lib/elements/Slide.svelte'
+  import SummaryRow from '$lib/elements/SummaryRow.svelte'
+  import { translate } from '$lib/i18n/translations'
+  import arrow from '$lib/icons/arrow'
+  import { listeningForAllInvoiceUpdates$, paymentUpdates$, settings$ } from '$lib/streams'
+  import { BitcoinDenomination } from '$lib/types'
+  import { createRandomHex, mainDomain } from '$lib/utils'
+  import Big from 'big.js'
+  import lightning from '$lib/lightning'
+
+  export let url: URL
+  export let k1: string
+  export let callback: string // The URL from LN SERVICE which will accept the pay request parameters
+  export let minWithdrawable: number // Min amount (in millisatoshis) the user can withdraw from LN SERVICE, or 0
+  export let maxWithdrawable: number // Max amount (in millisatoshis) the user can withdraw from LN SERVICE, or equal to minWithdrawable if the user has no choice over the amounts
+  export let defaultDescription: string // A default withdrawal invoice description
+
+  const serviceName = mainDomain(url.hostname)
+  const fixedAmountWithdrawable = minWithdrawable === maxWithdrawable
+
+  let slide = 0
+  let previousSlide = 0
+
+  let amount = ''
+  let amountError = ''
+
+  function back() {
+    slide -= 1
+  }
+
+  function next(increment = 1) {
+    slide += increment
+  }
+
+  function validateAmount() {
+    const valueSats =
+      amount && amount !== '0'
+        ? Big(
+            convertValue({
+              value: amount,
+              from: $settings$.primaryDenomination,
+              to: BitcoinDenomination.msats
+            }) as string
+          )
+        : Big(0)
+
+    if (valueSats.lt(minWithdrawable)) {
+      amountError = $translate('app.errors.min_withdrawable')
+    }
+
+    if (valueSats.gt(maxWithdrawable)) {
+      amountError = $translate('app.errors.max_withdrawable')
+    }
+
+    !amountError && next()
+  }
+
+  let requesting = false
+  let requestError = ''
+
+  async function initiateWithdraw() {
+    try {
+      const amountMsats = fixedAmountWithdrawable
+        ? minWithdrawable.toString()
+        : convertValue({
+            value: amount,
+            from: $settings$.primaryDenomination,
+            to: BitcoinDenomination.msats
+          })
+
+      requestError = ''
+      requesting = true
+
+      // create payment request
+      const lnApi = lightning.getLn()
+
+      const payment = await lnApi.createInvoice({
+        amount_msat: amountMsats === '0' ? 'any' : (amountMsats as string),
+        description: defaultDescription || `${serviceName} LNURL Withdraw`,
+        expiry: $settings$.invoiceExpiry,
+        label: createRandomHex()
+      })
+
+      // add to payments
+      paymentUpdates$.next(payment)
+
+      // if not listening for payments, track specific invoice
+      if (!$listeningForAllInvoiceUpdates$) {
+        lightning.waitForAndUpdatePayment(payment)
+      }
+
+      const url = new URL(callback)
+      const kind = url.searchParams.get('kind')
+
+      if (kind) {
+        url.searchParams.set('kind', 'withdrawRequest')
+      }
+
+      url.searchParams.set('k1', k1)
+      url.searchParams.set('pr', payment.bolt11 as string)
+
+      const result = await fetch(LNURL_PROXY, {
+        headers: {
+          'Target-URL': url.toString()
+        }
+      }).then((res) => res.json())
+
+      if (result.status === 'ERROR') {
+        throw new Error(result.reason)
+      }
+
+      goto(`/payments/${payment.id}`)
+    } catch (error) {
+      const { message } = error as Error
+      requestError = message
+      requesting = false
+    }
+  }
 </script>
+
+{#if slide === 0}
+  <Slide
+    back={() => {
+      goto('/')
+    }}
+    direction={previousSlide > slide ? 'right' : 'left'}
+  >
+    <section class="flex flex-col justify-center items-start w-full p-6 max-w-lg">
+      <h1 class="text-4xl font-bold mb-4">{$translate('app.headings.withdraw_request')}</h1>
+      <h2 class="text-2xl font-semibold mb-2">{serviceName}</h2>
+
+      <p class="text-neutral-600 dark:text-neutral-400 italic">
+        {defaultDescription}
+      </p>
+
+      <div class="mt-8 w-full">
+        <SummaryRow>
+          <span slot="label"
+            >{$translate(
+              fixedAmountWithdrawable
+                ? 'app.labels.fixed_withdrawable'
+                : 'app.labels.min_withdrawable'
+            )}</span
+          >
+          <span slot="value"
+            >{convertValue({
+              value: minWithdrawable.toString(),
+              from: BitcoinDenomination.msats,
+              to: $settings$.primaryDenomination
+            })}
+            {$settings$.primaryDenomination}</span
+          >
+        </SummaryRow>
+
+        {#if !fixedAmountWithdrawable}
+          <SummaryRow>
+            <span slot="label">{$translate('app.labels.max_withdrawable')}</span>
+            <span slot="value"
+              >{convertValue({
+                value: maxWithdrawable.toString(),
+                from: BitcoinDenomination.msats,
+                to: $settings$.primaryDenomination
+              })}
+              {$settings$.primaryDenomination}</span
+            >
+          </SummaryRow>
+        {/if}
+      </div>
+
+      <div class="mt-6 w-full">
+        <Button
+          text={$translate('app.buttons.next')}
+          on:click={() => next(fixedAmountWithdrawable ? 2 : 1)}
+        >
+          <div slot="iconRight" class="w-6 -rotate-90">
+            {@html arrow}
+          </div>
+        </Button>
+      </div>
+    </section>
+  </Slide>
+{/if}
+
+{#if slide === 1}
+  <Slide {back} direction={previousSlide > slide ? 'right' : 'left'}>
+    <Amount
+      direction="send"
+      bind:value={amount}
+      bind:error={amountError}
+      next={validateAmount}
+      hint={$translate('app.hints.pay_request', {
+        min: `${convertValue({
+          value: minWithdrawable.toString(),
+          from: BitcoinDenomination.msats,
+          to: $settings$.primaryDenomination
+        })} ${$settings$.primaryDenomination.toUpperCase()}`,
+        max: `${convertValue({
+          value: maxWithdrawable.toString(),
+          from: BitcoinDenomination.msats,
+          to: $settings$.primaryDenomination
+        })} ${$settings$.primaryDenomination.toUpperCase()}`
+      })}
+      minMsats={minWithdrawable.toString()}
+      maxMsats={maxWithdrawable.toString()}
+      required
+    />
+  </Slide>
+{/if}
+
+{#if slide === 2}
+  <Slide {back} direction={previousSlide > slide ? 'right' : 'left'}>
+    <Summary
+      type="payment_request"
+      direction="receive"
+      destination={serviceName}
+      description={defaultDescription}
+      value={fixedAmountWithdrawable
+        ? convertValue({
+            value: minWithdrawable.toString(),
+            from: BitcoinDenomination.msats,
+            to: $settings$.primaryDenomination
+          })
+        : amount}
+      {requesting}
+      expiry={null}
+      on:complete={initiateWithdraw}
+    />
+  </Slide>
+{/if}
+
+<div class="absolute bottom-0 p-4">
+  <ErrorMsg bind:message={requestError} />
+</div>
