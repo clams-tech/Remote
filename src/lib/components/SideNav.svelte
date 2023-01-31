f5edd9f54bc9b8989941766966499d6420d61e0f
johngribbin <30157175+johngribbin@users.noreply.github.com>
2023-01-30 20:54:00 -0500

    Updates

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -4,6 +4,9 @@
   import settingsIcon from '$lib/icons/settings'
   import graphIcon from '$lib/icons/graph'
   import { goto } from '$app/navigation'
+  import { userAgent } from '$lib/utils'
+
+  const device = userAgent!.getDevice()
 
   const links = [
     {
@@ -23,33 +26,23 @@
     }
   ]
 
-  let width = 12
   let openNav = false
-
-  function handleOpen() {
-    width = 36
-    openNav = true
-  }
-
-  function handleClose() {
-    width = 12
-    openNav = false
-  }
 </script>
 
-<div class={`w-${width} absolute top-5 left-4`}>
-  <!-- svelte-ignore a11y-mouse-events-have-key-events -->
-  <div class="flex flex-col" on:mouseover={handleOpen} on:mouseout={handleClose}>
+<div class="absolute top-5 left-4">
+  <div
+    class="flex flex-col"
+    on:mouseover={() => (openNav = true)}
+    on:mouseleave={() => (openNav = false)}
+  >
     {#each links as link}
       <div class="flex items-center">
-        <!-- svelte-ignore a11y-click-events-have-key-events -->
         <span class="mb-4 hover:cursor-pointer" on:click={() => goto(link.route)}>
-          <div in:fade out:fade class="w-8 mr-2">
+          <div in:fade class="w-8 mr-2">
             {@html link?.icon}
           </div>
         </span>
         {#if openNav}
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
           <span
             class="flex items-center mb-4 hover:cursor-pointer"
             on:click={() => goto(link.route)}
@@ -61,15 +54,15 @@
       </div>
     {/each}
   </div>
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div
-    in:fade
-    out:fade
-    class="w-8 ml-2 hover:cursor-pointer"
-    on:click={() => {
-      openNav = !openNav
-    }}
-  >
-    >>
-  </div>
+  {#if device.type === 'mobile'}
+    <div
+      in:fade
+      class="w-8 ml-2 hover:cursor-pointer"
+      on:click={() => {
+        openNav = !openNav
+      }}
+    >
+      >>
+    </div>
+  {/if}
 </div>
