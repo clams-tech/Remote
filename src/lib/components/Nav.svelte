d85334e906e2666e85b95b10a26398c10fcb9cff
Aaron <abarnard@protonmail.com>
2023-02-03 08:20:30 +1100

    Shadow on desktop only

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-  class="absolute group/sidebar flex md:tall:flex-col flex-row items-center justify-between md:tall:justify-start md:tall:items-start top-0 left-0 p-4 z-20 w-full md:tall:w-auto md:tall:h-full md:tall:border-r border-neutral-100 shadow-sm dark:border-neutral-700"
+  class="absolute group/sidebar flex md:tall:flex-col flex-row items-center justify-between md:tall:justify-start md:tall:items-start top-0 left-0 p-4 z-20 w-full md:tall:w-auto md:tall:h-full md:tall:border-r border-neutral-100 md:tall:shadow-sm dark:border-neutral-700"
 >
   <div class="w-16 md:tall:w-20">
     {@html clamsIcon}
