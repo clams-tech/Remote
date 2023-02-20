cbea9f06eb64b701947a7468ae21f8c9903a7e3b
johngribbin <30157175+johngribbin@users.noreply.github.com>
2023-02-20 17:21:12 -0500

    More PR review tweaks

--- src/routes/bkpr/components/AccountInsights.svelte
+++ src/routes/bkpr/components/AccountInsights.svelte
@@ -2,13 +2,13 @@
   import { convertValue } from '$lib/conversion'
   import { BitcoinDenomination } from '$lib/types'
   import { truncateValue } from '$lib/utils'
-  import { onDestroy, onMount } from 'svelte'
+  import { onDestroy } from 'svelte'
   import { channelsAPY$ } from '$lib/streams'
   import { translate } from '$lib/i18n/translations'
   import type { ChannelAPY } from '$lib/backends'
   import type { Chart, ChartItem } from 'chart.js'
 
-  let showZeroFeesMessage = false
+  let showComponent = false
   let feesChart: Chart<'pie', number[], string>
   let apyChart: Chart<'pie', number[], string>
 
@@ -16,6 +16,7 @@
     fees = 'fees',
     apy = 'apy'
   }
+
   const charts = [
     {
       id: ChartIDs.fees,
@@ -27,9 +28,13 @@
     }
   ]
 
-  $: net = channelsAPY$.value.data?.filter((item) => item.account === 'net')
-
-  $: channels = channelsAPY$.value.data?.filter((item) => item.account !== 'net')
+  $: ({ net, channels } = (channelsAPY$.value.data || []).reduce(
+    (acc, item) => {
+      acc[item.account === 'net' ? 'net' : 'channels'].push(item)
+      return acc
+    },
+    { net: [], channels: [] } as { net: ChannelAPY[]; channels: ChannelAPY[] }
+  ))
 
   async function renderFeesChart(channels: ChannelAPY[]) {
     const { Chart } = await import('chart.js/auto')
@@ -37,7 +42,7 @@
     let labels: string[] = []
     let data: number[] = []
 
-    channels?.forEach(({ account, fees_in_msat, fees_out_msat }) => {
+    channels.forEach(({ account, fees_in_msat, fees_out_msat }) => {
       const routingFees = fees_in_msat + fees_out_msat
 
       if (routingFees) {
@@ -85,7 +90,7 @@
     let labels: string[] = []
     let data: number[] = []
 
-    channels?.forEach(({ account, apy_total }) => {
+    channels.forEach(({ account, apy_total }) => {
       const total = Number(apy_total.substring(0, apy_total.length - 1))
 
       if (total) {
@@ -118,37 +123,35 @@
     }
   }
 
-  onMount(async () => {
+  $: {
     if (net && net[0]?.fees_in_msat + net[0]?.fees_out_msat === 0) {
-      showZeroFeesMessage = true
-      return
+      showComponent = false
+    } else {
+      renderFeesChart(channels)
+      renderApyChart(channels)
+      showComponent = true
     }
-
-    if (channels) {
-      await renderFeesChart(channels)
-      await renderApyChart(channels)
-    }
-  })
+  }
 
   onDestroy(() => {
-    feesChart.destroy()
-    apyChart.destroy()
+    feesChart && feesChart.destroy()
+    apyChart && apyChart.destroy()
   })
 </script>
 
-<section class="p-6 max-w-md">
-  <h1 class="text-4xl mb-6 font-bold">{$translate('app.headings.account_insights')}</h1>
-  <p>
-    {showZeroFeesMessage
-      ? $translate('app.subheadings.account_insights_fallback')
-      : $translate('app.subheadings.account_insights')}
-  </p>
-  <section class="mt-6 w-full flex justify-between flex-wrap gap-6">
-    {#each charts as { title, id }}
-      <div class="flex flex-col flex-wrap">
-        <h3 class="text-sm text-neutral-400">{title}</h3>
-        <canvas height="auto" width="auto" class="block" {id} />
-      </div>
-    {/each}
+{#if showComponent}
+  <section class="p-6 max-w-md">
+    <h1 class="text-4xl mb-6 font-bold">{$translate('app.headings.account_insights')}</h1>
+    <p>
+      {$translate('app.subheadings.account_insights')}
+    </p>
+    <section class="mt-6 w-full flex justify-between flex-wrap gap-6">
+      {#each charts as { title, id }}
+        <div class="flex flex-col flex-wrap">
+          <h3 class="text-sm text-neutral-400">{title}</h3>
+          <canvas height="auto" width="auto" class="block" {id} />
+        </div>
+      {/each}
+    </section>
   </section>
-</section>
+{/if}
