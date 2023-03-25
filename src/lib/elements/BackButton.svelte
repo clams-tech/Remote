eea2051485a99f217c9f7092e17dded61a018d64
Aaron <abarnard@protonmail.com>
2023-03-25 14:09:05 +1100

    Style tweaks

--- src/lib/elements/BackButton.svelte
+++ src/lib/elements/BackButton.svelte
@@ -7,7 +7,7 @@
 
 <button
   on:click
-  class="fixed top-0 z-10 left-1 flex items-center pt-5 pb-4 px-2 cursor-pointer font-bold text-inherit w-full bg-white dark:bg-neutral-900"
+  class="absolute top-0 z-10 left-0 flex items-center pt-5 pb-4 px-3 cursor-pointer font-bold text-inherit w-full bg-white dark:bg-neutral-900"
 >
   <div class="w-6 cursor-pointer rotate-90">
     {@html caret}
