1403b0c73df2c6c0ff01b3edc7799e9bbdb449c5
aaronbarnardsound <abarnard@protonmail.com>
2023-07-12 16:40:47 +1000

    Fix scroll

--- src/routes/connections/+page.svelte
+++ src/routes/connections/+page.svelte
@@ -13,43 +13,41 @@
 </script>
 
 <Section>
-  <SectionHeading icon={keys} />
+  <div class="h-full flex flex-col overflow-hidden">
+    <SectionHeading icon={keys} />
 
-  <!-- NO CONNECTIONS YET -->
-  {#if !$storedConnections$.length}
-    <Paragraph>
-      {@html $translate(`${translateBase}.introduction`)}
-    </Paragraph>
+    <!-- NO CONNECTIONS YET -->
+    {#if !$storedConnections$.length}
+      <Paragraph>
+        {@html $translate(`${translateBase}.introduction`)}
+      </Paragraph>
 
-    <a href="/connections/add" class="mt-6 no-underline block">
-      <Button text={$translate(`${translateBase}.add_connection`)}>
-        <div class="w-6 ml-1" slot="iconRight">{@html plus}</div>
-      </Button>
-    </a>
-  {:else}
-    <div
-      class="grid justify-center 2xl:max-w-2xl grid-cols-2 sm:grid-cols-3 gap-2 w-full max-w-xl overflow-auto mt-6"
-    >
-      {#each $storedConnections$ as { label, id, type }}
-        {@const connectionTypeDetails = connectionOptions.find((c) => c.type === type)}
-        <a
-          href={`/connections/${id}`}
-          class="aspect-square no-underline border p-4 border-neutral-600 rounded flex flex-col justify-center items-center hover:bg-neutral-800/90 bg-neutral-900 transition-all"
-        >
-          {#if connectionTypeDetails}
-            <div class="w-full">{@html connectionTypeDetails.icon}</div>
-          {/if}
-          <span class="font-semibold border rounded px-4 py-1 truncate w-full text-center"
-            >{label}</span
+      <a href="/connections/add" class="mt-6 no-underline block">
+        <Button text={$translate(`${translateBase}.add_connection`)}>
+          <div class="w-6 ml-1" slot="iconRight">{@html plus}</div>
+        </Button>
+      </a>
+    {:else}
+      <div class="flex flex-col gap-2 w-full flex-grow overflow-auto max-w-xl mt-4">
+        {#each $storedConnections$ as { label, id, type }}
+          {@const connectionTypeDetails = connectionOptions.find((c) => c.type === type)}
+          <a
+            href={`/connections/${id}`}
+            class="no-underline border p-4 border-neutral-600 rounded w-full flex justify-between items-center hover:bg-neutral-800/90 bg-neutral-900 transition-all"
           >
-        </a>
-      {/each}
-    </div>
+            <span class="font-semibold text-lg px-4 py-1 truncate">{label}</span>
+            {#if connectionTypeDetails}
+              <div class="w-32">{@html connectionTypeDetails.icon}</div>
+            {/if}
+          </a>
+        {/each}
+      </div>
 
-    <a href="/connections/add" class="mt-6 no-underline block">
-      <Button primary text={$translate(`${translateBase}.add_connection`)}
-        ><div class="w-6 ml-1" slot="iconRight">{@html plus}</div></Button
-      >
-    </a>
-  {/if}
+      <a href="/connections/add" class="mt-6 no-underline block">
+        <Button primary text={$translate(`${translateBase}.add_connection`)}
+          ><div class="w-6 ml-1" slot="iconRight">{@html plus}</div></Button
+        >
+      </a>
+    {/if}
+  </div>
 </Section>
