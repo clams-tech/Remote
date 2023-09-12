588ecfa71998eae83a8b5352c360001673243a04
aaronbarnardsound <abarnard@protonmail.com>
2023-09-12 10:09:43 +1000

    Add decode rune button and modal

--- src/lib/wallets/configurations/coreln/Index.svelte
+++ src/lib/wallets/configurations/coreln/Index.svelte
@@ -2,7 +2,7 @@
   import { translate } from '$lib/i18n/translations.js'
   import type { CoreLnConfiguration } from '$lib/@types/wallets.js'
   import TextInput from '$lib/components/TextInput.svelte'
-  import { simpleDeepClone } from '$lib/utils'
+  import { simpleDeepClone, truncateValue } from '$lib/utils'
   import { slide } from 'svelte/transition'
   import AdvancedConnection from './AdvancedConnection.svelte'
   import { createEventDispatcher, onMount } from 'svelte'
@@ -17,6 +17,12 @@
   import { validateNodeAddress } from '$lib/address.js'
   import type { Session } from '$lib/@types/session.js'
   import ShowMoar from '$lib/components/ShowMoar.svelte'
+  import { decode, type DecodedRune } from 'rune-decoder'
+  import { formatDateRelativeToNow } from '$lib/dates.js'
+  import Msg from '$lib/components/Msg.svelte'
+  import Modal from '$lib/components/Modal.svelte'
+  import SummaryRow from '$lib/components/SummaryRow.svelte'
+  import warning from '$lib/icons/warning.js'
 
   export let configuration: CoreLnConfiguration
 
@@ -82,6 +88,67 @@
   }
 
   onMount(() => focusConnectionInput && focusConnectionInput())
+
+  let formattedRestrictions: string
+  let decodedRune: DecodedRune | null
+  let decodeRuneError: string | null
+  let showDecodedRuneModal = false
+
+  const decodeRune = async () => {
+    decodeRuneError = null
+    decodedRune = decode(configurationUpdate.token)
+
+    if (decodedRune) {
+      formattedRestrictions = await Promise.all(
+        decodedRune.restrictions.map(async ({ summary }) => {
+          const alternatives = summary.split(' OR ')
+          const formattedAlternatives = await Promise.all(
+            alternatives.map(async (alternative) => {
+              let words = alternative.split(' ')
+              const lastIndex = words.length - 1
+              const lastValue = words[lastIndex]
+
+              // format rate limit
+              if (words[0] === 'rate') {
+                words = ['rate', 'limited', 'to', `${lastValue} requests per minute`]
+              }
+
+              // format id
+              if (words[0] === 'id') {
+                words[lastIndex] = truncateValue(lastValue)
+              }
+
+              // format time
+              if (words[0] === 'time') {
+                const timeMs = parseInt(lastValue) * 1000
+
+                words = ['valid', 'until', await formatDateRelativeToNow(timeMs / 1000)]
+              }
+
+              words = words.map((word, i) => {
+                // first or last word
+                if (i === 0 || i === words.length - 1) {
+                  word = `<b>${word}</b>`
+                }
+
+                return word
+              })
+
+              return words.join(' ')
+            })
+          )
+
+          return formattedAlternatives.join('<span class="text-xs mr-2"><i><br>OR<br></i></span>')
+        })
+      ).then((restrictions) =>
+        restrictions.join('<span class="text-xs mr-2"><i><br>AND<br></i></span>')
+      )
+
+      showDecodedRuneModal = true
+    } else {
+      decodeRuneError = $translate('app.errors.decode_rune')
+    }
+  }
 </script>
 
 <svelte:window on:keyup|stopPropagation={handleKeyPress} />
@@ -123,7 +190,7 @@
   </div>
 
   <!-- AUTHENTICATION -->
-  <div class="w-full mt-4">
+  <div class="w-full mt-4 relative">
     <TextInput
       name="token"
       type="text"
@@ -131,6 +198,14 @@
       placeholder={$translate('app.labels.rune_placeholder')}
       bind:value={configurationUpdate.token}
     />
+
+    {#if configurationUpdate.token}
+      <button
+        on:click={decodeRune}
+        class="absolute top-0 right-0 text-xs border rounded-lg px-2 font-semibold py-0.5 hover:bg-neutral-800 transition-colors"
+        >Decode rune</button
+      >
+    {/if}
 
     <div class="text-xs mt-2">
       <ShowMoar label={$translate('app.labels.rune_commands')}>
@@ -178,4 +253,46 @@
       </div>
     </div>
   {/if}
+
+  {#if decodeRuneError}
+    <div in:slide={{ axis: 'y' }}>
+      <Msg type="error" bind:message={decodeRuneError} />
+    </div>
+  {/if}
 </div>
+
+{#if showDecodedRuneModal && decodedRune}
+  <Modal on:close={() => (showDecodedRuneModal = false)}>
+    <div class="h-full overflow-auto w-full">
+      <h4 class="font-semibold mb-2 w-full text-2xl">{$translate('app.labels.rune_summary')}</h4>
+
+      <SummaryRow>
+        <span slot="label">{$translate('app.labels.id')}:</span>
+        <span slot="value">{decodedRune.id}</span>
+      </SummaryRow>
+
+      <SummaryRow>
+        <span slot="label">{$translate('app.labels.hash')}:</span>
+        <span slot="value">{truncateValue(decodedRune.hash)}</span>
+      </SummaryRow>
+
+      <SummaryRow baseline={!!decodedRune.restrictions.length}>
+        <span slot="label">{$translate('app.labels.restrictions')}:</span>
+        <p slot="value">
+          {#if decodedRune.restrictions.length === 0}
+            <div class="flex items-center">
+              <div class="w-4 mr-2 text-utility-pending">
+                {@html warning}
+              </div>
+              <span class="text-utility-pending">{$translate('app.labels.unrestricted')}</span>
+            </div>
+          {:else}
+            {#await formattedRestrictions then formatted}
+              {@html formattedRestrictions}
+            {/await}
+          {/if}
+        </p>
+      </SummaryRow>
+    </div>
+  </Modal>
+{/if}
