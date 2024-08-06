5de5b6c58eabb8b91e34071956d50dfc18e25a55
John Gribbin <30157175+johngribbin@users.noreply.github.com>
2024-08-06 06:11:19 +0100

    2.2.0-0.2.1: [FEAT]: CLBOSS UI (#269)
    * 2.2.0-0.1.0 - Feature: plugins tile and clboss UI
    
    * Add Clboss interface
    
    * Change get
    
    * Add plugins interface and logic to check for clboss installation
    
    * Add todos
    
    * Add types and clean up logic
    
    * Add routing and query param logic
    
    * Move clboss status request and todo list to clboss ui page
    
    * Add more config components to UI
    
    * Add tooltip component and make small design changes to plugin tile
    
    * Add Commands sections and style the tooltip component
    
    * Add modal for clboss status
    
    * Style channel candidates and fee rate field in status modal
    
    * More styling of status modal
    
    * Hook up two more clboss commands
    
    * Add disabled until time to ui
    
    * Add method for unmanaging channels and fetch of active channels
    
    * Add table for unmanage commands
    
    * Unmange updates
    
    * Clean up unmanged logic
    
    * Hook up unmanage function
    
    * Update status section and remove modal
    
    * Clean up
    
    * Update todos
    
    * Update plugins interface to include start and stop methods
    
    * Update comments
    
    * Improve feedback when switching node
    
    * Refactor and clean up
    
    * Add i18n updates
    
    * More translations
    
    * version bump
    
    * Fix issue of constant spinner when clboss is not active
    
    * Fix issue of flashing bitcoind and lightningd not synced errors

---
+++ src/lib/@types/plugins.ts
@@ -0,0 +1,54 @@
+export interface PluginListResponse {
+  command: 'list'
+  plugins: Plugin[]
+}
+
+export type Plugin = {
+  name: string
+  active: boolean
+  dynamic: boolean
+}
+
+export type ClbossStatus = {
+  channel_candidates: {
+    id: string
+    onlineness: number
+  }[]
+  internet: {
+    connection: 'online' | 'offline'
+    checking_connectivity: boolean
+  }
+  onchain_feerate: {
+    hi_to_lo: number
+    init_mid: number
+    lo_to_hi: number
+    last_feerate_perkw: number
+    judgment: 'high fees' | 'low fees'
+  }
+  peer_metrics:
+    | {
+        [key: string]: {
+          age: number
+          age_human: string
+          seconds_per_attempt: number
+          success_per_attempt: number | null
+          success_per_day: number
+          connect_rate: number
+          in_fee_msat_per_day: number
+          out_fee_msat_per_day: number
+        }
+      }
+    | Record<string, never> //empty object
+  should_monitor_onchain_funds: {
+    status: 'notice' | 'ignore'
+    now: number
+    now_human: string
+    disable_until: number
+    disable_until_human: string
+    comment: string
+  }
+  unmanaged: {
+    comment: ''
+    [key: string]: string
+  }
+}
