7357be45ad0fa126c68b9fd88b596685801a85e0
John Gribbin <30157175+johngribbin@users.noreply.github.com>
2024-08-07 07:10:55 +0100

    2.2.0-0.1.2: [FIX]: Update onchain address validation (#271)
    * 2.2.0-0.1.2: [FIX]: Remove onchain address validation
    
    * Remove log
    
    * Revert change
    
    * Add regex to parse onchain addresses

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -7,3 +7,6 @@ export const nodePublicKeyRegex = /^[0-9a-fA-F]{66}$/
 export const bolt11Regex = /^(lnbcrt|lnbc|lntbs|lntb)[a-zA-HJ-NP-Z0-9]{1,}$/
 export const nodeConnectRegex =
   /[0-9a-fA-F]{66}@(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9]):[0-9]{4,}/
+
+export const onchainRegex =
+  /^(1[a-km-zA-HJ-NP-Z1-9]{25,33}|3[a-km-zA-HJ-NP-Z1-9]{33}|bc1q[a-zA-HJ-NP-Z0-9]{38,58}|bc1p[a-zA-HJ-NP-Z0-9]{58})$/
