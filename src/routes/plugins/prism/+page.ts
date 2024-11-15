ecfa00f03be7a7aed3537f056305361138f62226
johngribbin <30157175+johngribbin@users.noreply.github.com>
2024-11-15 14:24:12 +0000

    fix: build issue

--- src/routes/plugins/prism/+page.ts
+++ src/routes/plugins/prism/+page.ts
@@ -1,8 +1,10 @@
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ url }) => {
-  const { searchParams } = url
-  const wallet = searchParams.get('wallet')
+  if (typeof window !== 'undefined') {
+    const { searchParams } = url
+    const wallet = searchParams.get('wallet')
 
-  return { wallet }
+    return { wallet }
+  }
 }
