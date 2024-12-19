3577639afb297c30913519f4b40bb2165e84ade5
johngribbin <30157175+johngribbin@users.noreply.github.com>
2024-12-19 09:28:01 +0000

    Update gh pages deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const dev = process.env.NODE_ENV === 'development'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,9 +11,6 @@ const config = {
     adapter: adapter({
       fallback: '200.html'
     }),
-    paths: {
-      base: dev ? '' : '/Remote' // Use '/Remote' for GitHub Pages
-    },
     csp: {
       mode: 'auto',
       directives: {
